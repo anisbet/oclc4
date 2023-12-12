5ad8a60468f7eafd50b5a483c2290bf72cf275e7
anisbet <andrew@dev-ils.com>
2023-12-12 17:03:23 -0500

    Added records reader to RecordManager.

---
+++ oclc4.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+###############################################################################
+#
+# Purpose: Update OCLC local holdings for a given library.
+# Date:    Mon 04 Dec 2023 07:46:56 PM EST
+# Copyright (c) 2023 Andrew Nisbet
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+
+from os.path import exists
+from os import linesep
+import argparse
+import sys
+from ws2 import WebService
+from datetime import datetime
+import json
+from record import Record
+
+VERSION='0.00.00'
+APP = 'hist2json'
+
+# Wrapper for the logger. Added after the class was written
+# and to avoid changing tests. 
+# param: message:str message to either log or print. 
+# param: to_stderr:bool if True and logger
+def logit(messages, level:str='info', timestamp:bool=False):
+    time_str = ''
+    if timestamp:
+        time_str = f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] "
+    if isinstance(messages, list):
+        if level == 'error':
+            for message in messages:
+                print(f"{time_str}*error, {message}")
+        else:
+            for message in messages:
+                print(f"{time_str}{message}")
+    else:
+        if level == 'error':
+            msg = f"{time_str}*error, {messages}"
+        else:
+            msg = f"{time_str}{messages}"
+        print(f"{msg}")
+
+class RecordManager:
+    # TODO: add encoding flag?
+    def __init__(self, ignoreTags:dict={}, encoding:str='utf-8'):
+        self.adds = []
+        self.deletes = []
+        self.matchs = []
+        self.ignore_tags = ignoreTags
+        # OCLC numbers that were rejected. These could be because there was an add 
+        # request, when OCLC already has the number listed as a holding, a delete
+        # that OCLC doesn't show as a holding, and records rejected because they
+        # match an of the ignoreTags. 
+        self.rejected = []
+        self.encoding = encoding
+        self.backup_prefix = 'oclc_update_'
+
+    # Reads flat records into a list from file. Add lists are always flat format records. 
+    def readFlatOrMrkRecords(self, fileName:str, debug:bool=False) ->list:
+        if not fileName:
+            logit(f"no flat or mrk records to read.")
+        if exists(fileName):
+            with open(fileName, encoding='utf-8', mode='rt') as flat_file:
+                lines = []
+                my_type = ''
+                for line in flat_file:
+                    if '*** DOCUMENT BOUNDARY ***' in line or '=LDR ' in line:
+                        # A new boundary means a new record so output any existing.
+                        if lines:
+                            # Remember all mrk or flat files are add or set holding records when reading from file.
+                            record = Record(data=lines, action='set', rejectTags=self.ignore_tags, encoding=self.encoding)
+                            if (debug):
+                                print(f"{record}")
+                            self.adds.append(record)
+                            lines = []
+                    lines.append(line.rstrip())
+            flat_file.close()
+            # Output the last record since there are no more doc boundaries to trigger that.
+            if lines:
+                record = Record(data=lines, action='set', rejectTags=self.ignore_tags, encoding=self.encoding)
+                if (debug):
+                    print(f"{record}")
+                self.adds.append(record)
+        else:
+            logit(f"**error, {fileName} is either missing or empty.")
+            sys.exit(1)
+
+    # Reads delete OCLC numbers into a list from file. 
+    def readDeleteList(self, fileName:str) ->list:
+        pass
+
+    # Reads the holding report from OCLC which includes all of the library's holdings. 
+    # This is used as a yardstick to compare adds and deletes, that is, the adds list
+    # is compared and only the records not on this list will be added. Similarly, if
+    # this list doesn't include a number that appears on the delete list, it is not 
+    # sent as a delete request.
+    def readHoldingsReport(self, csvfileName:str) ->list:
+        pass
+        
+    # Given an arbitrary but specific list of records remove those that 
+    # are part of a second list, and return the first list. 
+    def dedupLists(self, l1:list, l2:list, debug:bool=False) -> list:
+        pass
+    def setHoldings(self, debug:bool=False):
+        pass
+    def unsetHoldings(self, debug:bool=False):
+        pass
+    def matchFailedHoldings(self, debug:bool=False):
+        pass
+    def updateSlimFlat(self, flatFile:str=None, debug:bool=False):
+        pass
+
+    def dumpJson(self, fileName:str, data:dict):
+        with open(fileName, 'wt') as fp:
+            json.dump(data, fp)
+        fp.close()
+
+    def loadJson(self, fileName:str) -> dict:
+        fp = open(fileName, 'rt')
+        ret_dict = json.load(fp)
+        fp.close()
+        return ret_dict
+
+    # This method is for when the application is requested to close 
+    # because of a software or keyboard interrupt like <ctrl-c>. 
+    # It writes the existing state of update to backup files.
+    # param: debug:bool outputs any additional debug information while
+    #   while running. 
+    def _restore_(self, debug:bool=False):
+        if debug:
+            logit(f"restoring records from backup...")
+        self.adds = self.loadJson(f"{self.backup_prefix}adds.json", self.adds)
+        self.deletes = self.loadJson(f"{self.backup_prefix}deletes.json", self.deletes)
+        self.matches = self.loadJson(f"{self.backup_prefix}matches.json", self.matches)
+        if debug:
+            logit(f"done.")
+
+    # This method is for when the application is requested to close 
+    # because of a software or keyboard interrupt like <ctrl-c>. 
+    # It writes the existing state of update to backup files. 
+    # param: debug:bool outputs any additional debug information while
+    #   while running. 
+    def cleanUp(self, debug:bool=False):
+        if debug:
+            logit(f"saving records' state to backup...")
+        self.dumpJson(f"{self.backup_prefix}adds.json", self.adds)
+        self.dumpJson(f"{self.backup_prefix}deletes.json", self.deletes)
+        self.dumpJson(f"{self.backup_prefix}matches.json", self.matches)
+        if debug:
+            logit(f"done.")
+
+# Main entry to the application if not testing.
+def main(argv):
+    debug = False
+    parser = argparse.ArgumentParser(
+        prog = 'oclc',
+        usage='%(prog)s [options]' ,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='''\
+            Maintains holdings in OCLC WorldCat database.
+            ''',
+        epilog='''\
+    TODO: add documentation here.
+        '''
+    )
+    parser.add_argument('--add', action='store', metavar='[/foo/my_nums.flat|.mrk]', help='List of bib records to add in flat or mrk format.')
+    parser.add_argument('-d', '--debug', action='store_true', default=False, help='turn on debugging.')
+    parser.add_argument('--delete', action='store', metavar='[/foo/oclc_nums.lst]', help='List of OCLC numbers to delete from OCLC\'s holdings database.')
+    parser.add_argument('--report', action='store', metavar='[/foo/oclcholdingsreport.csv]', help='Holdings report from OCLC\'s database in CSV format.')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
+    
+    args = parser.parse_args()
+    if args.version:
+        print(f"{APP} version: {VERSION}")
+        sys.exit(0)
+    manager = RecordManager(debug=args.debug)
+    try:
+        manager.readFlatOrMrkRecords(add=args.add, debug=args.debug)
+        manager.readHoldingsReport(reportCsv=args.report)
+        manager.runUpdate(debug=args.debug)
+        
+    except KeyboardInterrupt:
+        print(f"Keyboard interrupt.")
+        manager.cleanUp(debug=args.debug)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    doctest.testfile("oclc4.tst")
---
+++ oclc4.tst
@@ -0,0 +1,119 @@
+Tests for RecordManager
+=======================
+
+>>> from oclc4 import RecordManager
+>>> from os.path import exists
+>>> import os
+
+Test readFlatOrMrkRecords
+-------------------------
+ 
+>>> recman = RecordManager()
+>>> recman.readFlatOrMrkRecords('test/testB.flat', debug=True)
+*** DOCUMENT BOUNDARY ***
+FORM=MUSIC
+.000. |ajm  0c a
+.001. |aocn779882439
+.003. |aOCoLC
+.005. |a20140415031115.0
+.007. |asd fungnnmmneu
+.008. |a120307s2012    cau||n|e|i        | eng d
+.035.   |a(Sirsi) o779882439
+.035.   |a(OCoLC)779882439
+.035.   |a(CaAE) o779882439
+.650.  0|aTelevision music|zUnited States.
+*** DOCUMENT BOUNDARY ***
+FORM=VM
+.000. |agm a0n a
+.001. |aocn782078599
+.003. |aOCoLC
+.005. |a20170720213947.0
+.007. |avd cvaizs
+.008. |a120330p20122011mdu598 e          vleng d
+.028. 40|aAMP-8773
+.035.   |a(OCoLC)782078599
+.035.   |a(CaAE) o782078599
+.040.   |aWC4|cWC4|dTEF|dIEP|dVP@|dUtOrBLW
+.999.   |hEPLZDVD
+
+
+Test logit
+----------
+>>> from oclc4 import logit
+>>> logit("Something happened")
+Something happened
+>>> bad_mojo = ['this happened', 'then this', 'and finally this of all things.']
+>>> logit(bad_mojo, level='error')
+*error, this happened
+*error, then this
+*error, and finally this of all things.
+
+
+The following tests will fail because they include timestamps which changes. 
+Recomment them out after testing.
+# >>> logit("Something happened", timestamp=True)
+# Something happened
+# >>> logit(bad_mojo, level='error', timestamp=True)
+# [2023-12-12 16:32:18] *error, this happened
+# [2023-12-12 16:32:18] *error, then this
+# [2023-12-12 16:32:18] *error, and finally this of all things.
+
+Test dumping json
+-----------------
+
+>>> d = [
+...     {
+...     "numberOfRecords": 1,
+...     "briefRecords": [
+...         {
+...             "oclcNumber": "1236899214",
+...             "title": "Cats!",
+...             "creator": "Erica S. Perl",
+...             "date": "2021",
+...             "machineReadableDate": "2021",
+...             "language": "eng",
+...             "generalFormat": "Book",
+...             "specificFormat": "PrintBook",
+...             "edition": "",
+...             "publisher": "Random House",
+...             "publicationPlace": "New York",
+...             "isbns": [
+...                 "9780593380321",
+...                 "0593380320",
+...                 "9780593380338",
+...                 "0593380339",
+...                 "9781544460291",
+...                 "1544460295"
+...             ],
+...             "issns": [],
+...             "mergedOclcNumbers": [
+...                 "1237101016",
+...                 "1237102366",
+...                 "1276783496",
+...                 "1276798734",
+...                 "1281721227",
+...                 "1285930950",
+...                 "1288704071",
+...                 "1289594397",
+...                 "1289941124",
+...                 "1305859745"
+...             ],
+...             "catalogingInfo": {
+...                 "catalogingAgency": "DLC",
+...                 "catalogingLanguage": "eng",
+...                 "levelOfCataloging": " ",
+...                 "transcribingAgency": "DLC"
+...             }
+...         }
+...     ]
+... }]
+>>> rm = RecordManager()
+>>> test_json_file = 'test.json'
+>>> rm.dumpJson(fileName=test_json_file, data=d)
+>>> exists(test_json_file)
+True
+>>> e = rm.loadJson(test_json_file)
+>>> d == e
+True
+>>> if d == e:
+...     os.unlink(test_json_file)
